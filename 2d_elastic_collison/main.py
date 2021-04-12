a777b3a6b9549c62c18d6e2df0fb6a24ca6f8d3a
Jearce <jessiearce42@gmail.com>
2021-04-11 23:26:14 -0500

    moved physic equations to physics.py

--- 2d_elastic_collison/main.py
+++ 2d_elastic_collison/main.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 
+import physics
+
 from molecule import Molecule
 
 pygame.init()
@@ -11,7 +13,7 @@ size = width, height = 320, 240
 black = 0, 0, 0
 
 molecule1 = Molecule(
-    center=vector(50, 50), velocity=vector(0.05, 0), radius=5, mass=0.2
+    center=vector(50, 50), velocity=vector(0.05, 0), radius=20, mass=0.2
 )
 
 molecule2 = Molecule(
@@ -20,47 +22,6 @@ molecule2 = Molecule(
 
 screen = pygame.display.set_mode(size)
 
-
-def collide(molecule1, molecule2):
-    diff = molecule1.center - molecule2.center
-    distance = (diff.x ** 2 + diff.y ** 2) ** 0.5
-    return distance < (molecule1.radius + molecule1.radius)
-
-
-def calculate_v1(*, m1, m2, v1, v2):
-    if m1 == m2:
-        return v2
-
-    left = ((m1 - m2) / (m1 + m2)) * v1
-    right = ((2 * m2) / (m1 + m2)) * v2
-    return left + right
-
-
-def calculate_v2(*, m1, m2, v1, v2):
-    if m1 == m2:
-        return v1
-
-    left = ((2 * m1) / (m1 + m2)) * v1
-    right = ((m2 - m1) / (m1 + m2)) * v2
-    return left + right
-
-
-def calculate_velocities(molecule1, molecule2):
-    v1 = calculate_v1(
-        m1=molecule1.mass,
-        m2=molecule2.mass,
-        v1=molecule1.velocity,
-        v2=molecule2.velocity,
-    )
-    v2 = calculate_v2(
-        m1=molecule1.mass,
-        m2=molecule2.mass,
-        v1=molecule1.velocity,
-        v2=molecule2.velocity,
-    )
-    return v1, v2
-
-
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -68,8 +29,8 @@ while 1:
 
     screen.fill(black)
 
-    if collide(molecule1, molecule2):
-        v1, v2 = calculate_velocities(molecule1, molecule2)
+    if physics.collide(molecule1, molecule2):
+        v1, v2 = physics.calculate_velocities(molecule1, molecule2)
         molecule1.velocity = vector(v1, molecule1.velocity.y)
         molecule2.velocity = vector(v2, molecule2.velocity.y)
 
