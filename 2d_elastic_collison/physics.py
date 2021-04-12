bca719ff0a64fb2b6012209ca3ddad04d4946247
Jearce <jessiearce42@gmail.com>
2021-04-12 00:42:42 -0500

    refactored physics module to account for 2d ellastic collision

--- 2d_elastic_collison/physics.py
+++ 2d_elastic_collison/physics.py
@@ -1,38 +1,34 @@
-def collide(molecule1, molecule2):
+from pygame.math import Vector2
+
+from molecule import Molecule
+
+
+def collide(molecule1, molecule2) -> bool:
     diff = molecule1.center - molecule2.center
     distance = (diff.x ** 2 + diff.y ** 2) ** 0.5
     return distance < (molecule1.radius + molecule2.radius)
 
 
-def calculate_v1(*, m1, m2, v1, v2):
-    if m1 == m2:
-        return v2
+def compute_collision_velocity(*, molecule1, molecule2) -> Vector2:
+    masses = (2 * molecule2.mass) / (molecule1.mass + molecule2.mass)
+    velocity_diff: Vector2 = molecule1.velocity - molecule2.velocity
+    position_diff: Vector2 = molecule1.center - molecule2.center
+    dot_product: Vector2 = velocity_diff * position_diff
+    magnitude_squared: Vector2 = position_diff.magnitude_squared()
+    return (
+        molecule1.velocity
+        - masses * (dot_product / magnitude_squared) * position_diff
+    )
 
-    left = ((m1 - m2) / (m1 + m2)) * v1
-    right = ((2 * m2) / (m1 + m2)) * v2
-    return left + right
 
+def calculate_v(*, molecule1: Molecule, molecule2: Molecule) -> Vector2:
+    if molecule1.mass == molecule2.mass:
+        return molecule2.velocity
 
-def calculate_v2(*, m1, m2, v1, v2):
-    if m1 == m2:
-        return v1
-
-    left = ((2 * m1) / (m1 + m2)) * v1
-    right = ((m2 - m1) / (m1 + m2)) * v2
-    return left + right
+    return compute_collision_velocity(molecule1=molecule1, molecule2=molecule2)
 
 
 def calculate_velocities(molecule1, molecule2):
-    v1 = calculate_v1(
-        m1=molecule1.mass,
-        m2=molecule2.mass,
-        v1=molecule1.velocity,
-        v2=molecule2.velocity,
-    )
-    v2 = calculate_v2(
-        m1=molecule1.mass,
-        m2=molecule2.mass,
-        v1=molecule1.velocity,
-        v2=molecule2.velocity,
-    )
+    v1 = calculate_v(molecule1=molecule1, molecule2=molecule2)
+    v2 = calculate_v(molecule1=molecule2, molecule2=molecule1)
     return v1, v2
